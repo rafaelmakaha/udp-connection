113545d018370b94379c5d2860886e5d01e177b0
Rafael Makaha <rafael.makaha@gmail.com>
2019-09-20 02:17:19 -0300

    Adds message with BUG.
    Signed-off-by: Rafael Makaha <rafael.makaha@gmail.com>

--- udp_connection.c
+++ udp_connection.c
@@ -40,28 +40,37 @@ int main(int argc, char **argv){
 	char buffer[MAX];									// Buffer to Hold package
 	
 	memset(buffer, 0, MAX);
-
 	// Pointers on the Buffer -------------------------------------------------------
 	udp_header *datagram = (udp_header *) buffer;
 
-	datagram = calloc(1, sizeof(udp_header));
-
 	// Sets udp_header Values ---------------------------------------------------------
 	datagram->query_id = htons(rand());
-	datagram->flags = 0x0100;
-	datagram->question_count = htons(1);
-	datagram->ans_RR = 0x0000;
-	datagram->auth_RR = 0x0000;
-	datagram->add_RR = 0x0000;
-	datagram->query = htons(1);
+	datagram->flags = htons(0x0100);
+	datagram->question_count = htons(0x0001);
+	datagram->ans_RR = htons(0);
+	datagram->auth_RR = htons(0);
+	datagram->add_RR = htons(0);
+	datagram->query = htons(0x0001);
+
+	// Transforms Message Format ----------------------------------------------------
+	// char * message_formatted = (char *) (malloc(sizeof(strlen(message) + 1)));
+	// int count = 0;
+	// for(int i=0; i < strlen(message); i++){
+	// 	if(message[i] == '.'){
+	// 		message_formatted[i-count] = count;
+	// 	}
+	// 	message_formatted[i+1] = message[i];
+	// 	count++;
+	// 	printf("%d %s\n", count, message_formatted);
+	// }
 
 	// Sets Data Information Values --------------------------------------------------
-	char * data = (buffer + sizeof(datagram));				// Pointer do the beggining of payload
+	char * data = (buffer + sizeof(udp_header));			// Pointer do the beggining of payload
 	strcpy(data, message);									// Copy message to payload
 	int len = strlen(data)+1;								// Length of message
-	infos * end = (infos *) (data+len);						//
-	end->type = htons(1);
-	end->class = htons(1);
+	infos * end = (infos *) (data+len);
+	end->type = htons(0x0001);
+	end->class = htons(0x0001);
 
 	// Creates Socket  ------------------------------------------------------------
 	if((socket_fd = socket(AF_INET, SOCK_DGRAM, 0))<0){
@@ -69,16 +78,17 @@ int main(int argc, char **argv){
 		return 0;
 	}
 
+	// Configures Socket ----------------------------------------------------------
 	target.sin_family = AF_INET;
 	target.sin_port = htons(PORT);
 	target.sin_addr.s_addr = inet_addr(server_address);
 
-	// Checks Buffer Length -----------------------------------------------
-	const int packege_length = sizeof(datagram) + sizeof(end);
+	// Checks Buffer Length --------------------------------------------------------
+	const int packege_length = sizeof(udp_header) + len +sizeof(infos);
 
-	// Sends Package -----------------------------------------------------
+	// Sends Package ---------------------------------------------------------------
 	int x = sendto(socket_fd, buffer,
-		packege_length, 0, (const struct sockaddr *) &target,
+		packege_length, 0, (struct sockaddr *) &target,
 		sizeof(target));
 
 	close(socket_fd);
